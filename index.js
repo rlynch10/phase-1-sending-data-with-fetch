9bfc142e958aead980179c0e395422c44167d047
Robert Lynch <relynch10@gmail.com>
2023-05-07 20:56:26 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(firstName, email){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email,
+        })
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(error => document.querySelector("body").innerHTML = error)
+    }
+
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID
+}
+submitData("name", "name@name.com")
